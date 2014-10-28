fab216fc46054119bc98f1ffb04b5180d5565d05
wisard1862 <danieswain@gmail.com>
2014-10-28 15:53:38 -0400

    Updated Test

--- src/TestSOM.java
+++ src/TestSOM.java
@@ -11,18 +11,19 @@ public class TestSOM {
     
     public static void main(String[] args){
         SetCreatorEnhanced som = new SetCreatorEnhanced();
-//        som.run();
-//        som.train(10);
+        Intersector thing = new Intersector();
+        som.run();
+        som.train(10);
+        thing.read(10);
+        thing.run();
+        thing.resultsToFile("result");
+        thing.totalResultsFile();
 //        som.runForSpecialSet();
 //        som.trainAllButOneRecord();
-        Intersector thing = new Intersector();
-//        thing.read(10);
-//        thing.run();
-        thing.readSpecialCase();
-        thing.run_2();
-//        thing.resultsToFile("result");
-//        thing.totalResultsFile();
-        thing.specialTotalResultsFile();
+//        thing.readSpecialCase();
+//        thing.run_2();
+//        thing.specialTotalResultsFile();
+        
     }
         
 }
