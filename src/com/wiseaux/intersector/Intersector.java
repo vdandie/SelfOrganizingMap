5ba1b457e485beed322b1cf4d123948d939aa0d6
wisard1862 <danieswain@gmail.com>
2014-10-28 15:56:49 -0400

    Updated Intersector

--- src/com/wiseaux/intersector/Intersector.java
+++ src/com/wiseaux/intersector/Intersector.java
@@ -32,7 +32,12 @@ public class Intersector {
      * Holds results for each of the runs.
      */
     ArrayList[] results;
-
+    
+    /**
+     * Holds the true/false pos/neg values throughout the tests.
+     */
+    Collective col;
+    
     /**
      * Holds the average results
      */
@@ -42,12 +47,24 @@ public class Intersector {
      * Number of files to go through.
      */
     protected int times;
+    
+    /**
+     * String formats.
+     */
+    String tableFormat = "%-10s | %-7s | %-7s |";
+    String line = "-------------------------------";
+    
+    /**
+     * Overall
+     */
+    String overall;
 
     public Intersector() {
         testSets = new Queue[11];
         trainingSets = new Queue[11];
         results = new ArrayList[11];
         averages = new ArrayList<>();
+        col = new Collective();
     }
 
     /**
@@ -92,11 +109,27 @@ public class Intersector {
             }
             runAlgorithm(i);
         }
+        
+        overall = "\n" + String.format(tableFormat, " ", "1", "0") + "\n" + line
+                + "\n" + String.format(tableFormat, "1", col.truePos, col.falsePos)
+                + "\n" + String.format(tableFormat, "0",  col.falseNeg, col.trueNeg)
+                + "\n" + line
+                + "\n" + String.format(tableFormat, " ",
+                        col.truePos+ "/" + (col.falseNeg+col.truePos),
+                        col.trueNeg+"/"+(col.trueNeg+col.falsePos))
+                + "\n" + String.format(tableFormat, " ",
+                        String.format("%.2f",
+                                ((double)col.truePos / ((double)col.falseNeg + col.truePos))),
+                        String.format("%.2f", 
+                                ((double)col.trueNeg) / ((double)col.trueNeg + col.falsePos)))
+                + "\n" + line
+                + "\n" + "Total Not Predictable : " + col.notPredictable
+        ;
     }
 
     
     /**
-     * Runs the tests.
+     * Runs the tests for the k-1 case
      */
     public void run_2() {
         if (testSets.length < 1 || trainingSets.length < 1) {
@@ -114,6 +147,22 @@ public class Intersector {
             }
             runAlgorithm(i);
         }
+        
+        overall = "\n" + String.format(tableFormat, " ", "1", "0") + "\n" + line
+                + "\n" + String.format(tableFormat, "1", col.truePos, col.falsePos)
+                + "\n" + String.format(tableFormat, "0",  col.falseNeg, col.trueNeg)
+                + "\n" + line
+                + "\n" + String.format(tableFormat, " ",
+                        col.truePos+ "/" + (col.falseNeg+col.truePos),
+                        col.trueNeg+"/"+(col.trueNeg+col.falsePos))
+                + "\n" + String.format(tableFormat, " ",
+                        String.format("%.2f",
+                                ((double)col.truePos / ((double)col.falseNeg + col.truePos))),
+                        String.format("%.2f", 
+                                ((double)col.trueNeg) / ((double)col.trueNeg + col.falsePos)))
+                + "\n" + line
+                + "\n" + "Total Not Predictable : " + col.notPredictable
+        ;
     }
     
     /**
@@ -129,80 +178,62 @@ public class Intersector {
 
         int count = 0, outerCount = 0;
         double temp;
-        ArrayList<Competitor> comp = new ArrayList<>();
+        
         for (Record test : testQueue) {
+            
             double min = Double.MAX_VALUE;
-            int name = 0;
-            boolean match = false;
-
+            boolean match;
+            
+            ArrayList<Competitor> comp = new ArrayList<>();
             for (Record rule : trainingQueue) { //For each test, send it against every rule
                 //and get the sumAbsDif. 
                 temp = sumAbsDif(test, rule);
                 if (temp < min) {
                     min = temp;
-                    name =  rule.getIntName();
+                    comp.clear(); //New minimum found, remove all
+                    
                     match = test.hasSameDecision(rule);
-                    comp.add(new Competitor(min,rule));
-                }
-            }
-            
-            //Initialize indexes
-            int[] indexes = new int[comp.size()];
-            for (int i = 0; i < indexes.length; i++) {
-                indexes[i] = -1;
-            }
-            
-            //Count the records with the same min
-            int minCounter = 0;
-            for (int i = 0; i < comp.size(); i++) {
-                if (comp.get(i).min == min) {
-                    minCounter++;
-                    indexes[i] = i;
-                } else {
-                    indexes[i] = -1;
+                    comp.add(new Competitor(min,rule, match));
+                } else if(temp == min) {
+                    match = test.hasSameDecision(rule);
+                    comp.add(new Competitor(min,rule, match));
                 }
             }
             
             boolean predictable = true;
-            if (minCounter > 1) {
+            if (comp.size() > 1) {
                 int dec0 = 0, dec1 = 0;
-                for (int i : indexes) {
-                    if (i == -1) {
-                        //Do nothing
-                    } else if (comp.get(i).rec.getDecision() == 0) {
+                for(Competitor dec : comp) {
+                    if(dec.dec == 0) {
                         dec0++;
                     } else {
                         dec1++;
                     }
-                    name = i;
                 }
-
+                
                 if (dec0 > dec1) { //Take the dominant decision
-                    name = comp.get(name).rec.getIntName();
-                    match = test.getDecision() == 0;
-                    comp.get(comp.size() - 1 ).dec = 0;
+                    comp.get(0).dec = 0;
+                    comp.get(0).match = comp.get(0).dec == test.getDecision();
                 } else if (dec0 < dec1) {
-                    name = comp.get(name).rec.getIntName();
-                    match = test.getDecision() == 1;
-                    comp.get(comp.size() - 1 ).dec = 1;
-                } else if (dec0 == dec1) {
+                    comp.get(0).dec = 1;
+                    comp.get(0).match = comp.get(0).dec == test.getDecision();
+                } else if (dec0 == dec1) { //If no dominant decision, not predictable
                     predictable = false;
                 }
-
             } 
             
-            if (match && predictable) { // Matched and predictable
+            if (comp.get(0).match && predictable) { // Matched and predictable
                 count++;
                 outerCount++;
-                if (test.getDecision() == 0) {
+                if (test.getDecision() == 1) {
                     truePos += 1;
                 } else {
-                    falsePos += 1;
+                    trueNeg += 1;
                 }
             } else if (predictable) { // Not matched, yet predictable
                 outerCount++;
-                if (test.getDecision() == 1) {
-                    trueNeg += 1;
+                if (test.getDecision() == 0) {
+                    falsePos += 1;
                 } else {
                     falseNeg += 1;
                 }
@@ -212,11 +243,11 @@ public class Intersector {
                 result.add(String.format("Test Record : %-6s | Min Value: %-3s "
                         + "| Rule : %-5s | Match: %-5s | Test %-6s",
                         String.format("%-5s", test.getName()),
-                        String.format("%.2f", min),
-                        String.format("R:%2s", name),
-                        String.format("%s", match),
-                        String.format("D: %.0f", test.getDecision()))
-                        + " : " + comp.get(comp.size()-1).rec.getDecision()
+                        String.format("%.2f", comp.get(0).min),
+                        String.format("R:%2s", comp.get(0).rec.getIntName()),
+                        String.format("%s", comp.get(0).match),
+                        String.format("TD: %.0f", test.getDecision()))
+                        + "RD : " + comp.get(0).dec
                         + "\n"
                 );
             } else {
@@ -229,15 +260,22 @@ public class Intersector {
         }
 
         //True/False Pos/Neg table
-        String tableFormat = "%-10s | %-6s | %-6s |";
-        String line = "-------------------------------";
         result.add(String.format(tableFormat, " ", "1", "0") + "\n" + line
-                + "\n" + String.format(tableFormat, "1", falsePos, falseNeg)
-                + "\n" + String.format(tableFormat, "0",  trueNeg, truePos)
+                + "\n" + String.format(tableFormat, "1", truePos, falsePos)
+                + "\n" + String.format(tableFormat, "0", falseNeg, trueNeg)
                 + "\n" + line
-                + "\n" + String.format(tableFormat, " ", falsePos+ "/" + (trueNeg+falsePos), truePos+"/"+(falseNeg+truePos))
+                + "\n" + String.format(tableFormat, " ",
+                        truePos +  "/" + (truePos+falseNeg),
+                        trueNeg +  "/" + (trueNeg+falsePos))
                 + "\n" + line
         );
+        
+        // Collect all of true/false pos/neg information
+        col.falsePos += falsePos;
+        col.falseNeg += falseNeg;
+        col.truePos += truePos;
+        col.trueNeg += trueNeg;
+        col.notPredictable += notPred;
 
         result.add("\nOverall Match percentage:  "
                 + String.format("%.2f", ((double) count / (double) outerCount) * 100)
@@ -361,7 +399,7 @@ public class Intersector {
     }
 
     
-        /**
+    /**
      * Reads the testSet file
      */
     private void readSpecialTestSetFile() {
@@ -490,6 +528,7 @@ public class Intersector {
             }
             write.write("\n\nTotal average match percentage: "
                     + String.format("%.2f", calcAvg() * 100) + "%");
+            write.write(overall);
             write.close();
 
         } catch (FileNotFoundException e) {
@@ -536,6 +575,7 @@ public class Intersector {
             }
             write.write("\n\nTotal average match percentage: "
                     + String.format("%.2f", calcAvg() * 100) + "%");
+            write.write(overall);
             write.close();
 
         } catch (FileNotFoundException e) {
@@ -554,14 +594,25 @@ class Competitor {
     double min;
     Record rec;
     double dec;
+    boolean match;
 
     Competitor() {
 
     }
 
-    Competitor(double min, Record rec) {
+    Competitor(double min, Record rec, boolean match) {
         this.min = min;
         this.rec = rec;
         this.dec = rec.getDecision();
+        this.match = match;
+    }
+}
+
+class Collective {
+    
+    int falsePos, falseNeg, truePos, trueNeg, notPredictable;
+    
+    Collective() {
+        falsePos = falseNeg = truePos = trueNeg = notPredictable = 0;
     }
 }
